020411581a49c0010ec94eb178123d7363e6f35d
xwx <huoergai@hotmail.com>
2023-10-25 15:29:30 +0800

    1.movie item dynamic color.

--- app/src/main/java/com/huoergai/muse/ui/view/viewholder/MovieViewHolder.kt
+++ app/src/main/java/com/huoergai/muse/ui/view/viewholder/MovieViewHolder.kt
@@ -3,6 +3,9 @@ package com.huoergai.muse.ui.view.viewholder
 import android.view.View
 import android.widget.RatingBar
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.palette.graphics.Palette
 import coil.load
 import com.google.android.material.textview.MaterialTextView
 import com.huoergai.muse.R
@@ -15,6 +18,7 @@ import com.huoergai.muse.model.entity.Movie
  * DES:
  */
 class MovieViewHolder(itemView: View) : BaseViewHolder(itemView) {
+    private val llc = itemView.findViewById<LinearLayoutCompat>(R.id.ll_bg)
     private val ivPoster = itemView.findViewById<AppCompatImageView>(R.id.iv_poster)
     private val mtvTitle = itemView.findViewById<MaterialTextView>(R.id.mtv_title)
     private val ratingBar = itemView.findViewById<RatingBar>(R.id.rating_bar)
@@ -24,8 +28,15 @@ class MovieViewHolder(itemView: View) : BaseViewHolder(itemView) {
         ratingBar.rating = movie.vote_average / 2f
 
         ivPoster.load(Cook.buildImageUrl(movie.poster_path)) {
+            allowHardware(false)
             crossfade(true)
-            // transformations(CircleCropTransformation())
+            listener(onSuccess = { _, result ->
+                Palette.Builder(result.drawable.toBitmap()).generate { p ->
+                    p?.darkVibrantSwatch?.rgb?.let {
+                        llc.setBackgroundColor(it)
+                    }
+                }
+            })
         }
     }
 }
