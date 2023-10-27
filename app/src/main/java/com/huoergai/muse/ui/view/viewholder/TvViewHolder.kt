18699bb185ea468878b7c2ef34891d8695b94e9b
xwx <huoergai@hotmail.com>
2023-10-27 16:17:24 +0800

    1.tv item poster.

--- app/src/main/java/com/huoergai/muse/ui/view/viewholder/TvViewHolder.kt
+++ app/src/main/java/com/huoergai/muse/ui/view/viewholder/TvViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatRatingBar
 import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.google.android.material.textview.MaterialTextView
 import com.huoergai.muse.R
 import com.huoergai.muse.base.BaseViewHolder
@@ -14,7 +15,7 @@ import com.huoergai.muse.model.entity.Tv
  * D&T: 2023-10-25 15:33
  * DES:
  */
-class TvViewHolder(itemView: View) : BaseViewHolder(itemView) {
+class TvViewHolder(private val itemView: View) : BaseViewHolder(itemView) {
     private val sivPoster = itemView.findViewById<AppCompatImageView>(R.id.iv_poster)
     private val mtvName = itemView.findViewById<MaterialTextView>(R.id.mtv_name)
     private val mtvDate = itemView.findViewById<MaterialTextView>(R.id.mtv_date)
@@ -25,7 +26,9 @@ class TvViewHolder(itemView: View) : BaseViewHolder(itemView) {
         mtvDate.text = tv.first_air_date
         ratingBar.rating = tv.vote_average / 2f
 
-        sivPoster.load(Cook.buildPosterUrl(tv.poster_path))
+        sivPoster.load(Cook.buildPosterUrl(tv.poster_path)) {
+            transformations(RoundedCornersTransformation(sivPoster.resources.getDimension(R.dimen.corner_radius_small)))
+        }
     }
 
 }
